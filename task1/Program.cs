9aae7a2e5cb6e5c1a4fcc8f28721a5b5c3f1b181
tynchtyk1985 <turganbekov1985@mail.ru>
2024-02-15 20:32:30 +0600

    add lesson14.02:task1

---
+++ task1/Program.cs
@@ -0,0 +1,51 @@
+﻿int[,] matrix = new int[3, 5];
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = rnd.Next(1, 11);
+    }
+}
+
+
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+int[,] CreateMatrix(int rowCount, int columnsCount)
+{
+    int[,] matrix = new int[rowCount, columnsCount];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] _ = CreateMatrix(4, 5);
+ShowMatrix(matrix);
